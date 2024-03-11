ada972d4db79ed3c91a58d005fedefac17047914
HiddenGrizzly <153844656+BrownyGrizzly@users.noreply.github.com>
2024-03-11 10:28:36 +0700

    Go

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        $categories = Category::all();
+        return view('auth.register', compact('categories'));
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'required|in:reader,editor,writer',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+
+        $user->categories()->attach($request->category_id);
+
+        return redirect('/login');
+    }
+}
